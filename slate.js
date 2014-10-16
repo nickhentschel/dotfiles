2f19c66f2fb03e8c1eeb784d0155ab5e2f550392
Nick Hentschel <hentschel.nicholas@gmail.com>
2014-10-16 12:07:43 -0400

    edited vimrc and slate.js

--- slate.js
+++ slate.js
@@ -1,48 +1,19 @@
 var relaunch = slate.operation('relaunch');
 
-// var nudgeRight = slate.operation('nudge', {
-//     'x' : '+10%',
-//     'y' : '+0'
-// });
-//
-// var nudgeLeft = slate.operation('nudge', {
-//     'x' : '-10%',
-//     'y' : '+0'
-// });
-//
-// var nudgeUp = slate.operation('nudge', {
-//     'x' : '+0',
-//     'y' : '-10%'
-// });
-//
-// var nudgeDown = slate.operation('nudge', {
-//     'x' : '+0',
-//     'y' : '+10%'
-// });
-//
-// slate.bind('right:ctrl;alt', function(win){
-//     var screenwidth = win.screen().rect().x;
-//     var rightcorner = win.topLeft().x + win.size().width;
-//     if(rightcorner <= 1920) {
-//         win.doOperation(nudgeRight);
-//     }
-//     // win.doOperation(nudgeRight);
-// });
-
 slate.bindAll({
-    '1:ctrl' : relaunch,
+    // '3:ctrl' : relaunch,
     'esc:ctrl' : S.op('grid'),
 
     // Resize Bindings
     // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-    'right:ctrl' : S.op('resize', { 'width' : '+10%', 'height' : '+0' }),
-    'left:ctrl' : S.op('resize', { 'width' : '-10%', 'height' : '+0' }),
-    'up:ctrl' : S.op('resize', { 'width' : '+0', 'height' : '-10%' }),
-    'down:ctrl' : S.op('resize', { 'width' : '+0', 'height' : '+10%' }),
-    'right:alt' : S.op('resize', { 'width' : '-10%', 'height' : '+0', 'anchor' : 'bottom-right' }),
-    'left:alt' : S.op('resize', { 'width' : '+10%', 'height' : '+0', 'anchor' : 'bottom-right' }),
-    'up:alt' : S.op('resize', { 'width' : '+0', 'height' : '+10%', 'anchor' : 'bottom-right' }),
-    'down:alt' : S.op('resize', { 'width' : '+0', 'height' : '-10%', 'anchor' : 'bottom-right' }),
+    // 'right:ctrl' : S.op('resize', { 'width' : '+10%', 'height' : '+0' }),
+    // 'left:ctrl' : S.op('resize', { 'width' : '-10%', 'height' : '+0' }),
+    // 'up:ctrl' : S.op('resize', { 'width' : '+0', 'height' : '-10%' }),
+    // 'down:ctrl' : S.op('resize', { 'width' : '+0', 'height' : '+10%' }),
+    // 'right:alt' : S.op('resize', { 'width' : '-10%', 'height' : '+0', 'anchor' : 'bottom-right' }),
+    // 'left:alt' : S.op('resize', { 'width' : '+10%', 'height' : '+0', 'anchor' : 'bottom-right' }),
+    // 'up:alt' : S.op('resize', { 'width' : '+0', 'height' : '+10%', 'anchor' : 'bottom-right' }),
+    // 'down:alt' : S.op('resize', { 'width' : '+0', 'height' : '-10%', 'anchor' : 'bottom-right' }),
 
     // Push Bindings
     // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
