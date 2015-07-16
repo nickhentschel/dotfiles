530ee6eb777024ff2065b04d7a83c004eb375e0d
Nick Hentschel <nhentschel@wayfair.com>
2015-07-16 13:08:13 -0400

    removed fish stuff and updated zshrc

--- fish_functions/fish_user_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-vim .config/fish/functions/fish_user_key_bindings.fish
-
-function fish_user_key_bindings.fish
-  fish_vi_key_bindings
-
-  bind -M insert -m default jk backward-char force-repaint
-end
